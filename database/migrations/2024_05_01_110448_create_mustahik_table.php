d420f9753697ce72f3715c83623067c5838d2abe
hanief <hanifadaadaaja@gmail.com>
2024-05-01 18:09:42 +0700

    final migration database

---
+++ database/migrations/2024_05_01_110448_create_mustahik_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mustahik', function (Blueprint $table) {
+            $table->id('id_mustahik');
+            $table->unsignedBigInteger('id_kategori')->index();
+            $table->enum('kondisi_rumah', [1,2,3,4,5]);
+            $table->integer('pendapatan_bulanan');
+            $table->integer('pengeluaran_bulanan');
+            $table->integer('jumlah_hutang');
+            $table->boolean('verifikasi');
+            $table->timestamps();
+
+            $table->foreign('id_kategori')->references('id_kategori')->on('kategori_mustahik');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mustahik');
+    }
+};
