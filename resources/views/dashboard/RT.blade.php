f47b378223c566bc80b6ba63be1e4f0779d06e27
Fahridanaa <fahridanaaa@gmail.com>
2024-05-08 00:21:38 +0700

    style: style graph in dashboard RT

--- resources/views/dashboard/RT.blade.php
+++ resources/views/dashboard/RT.blade.php
@@ -23,21 +23,16 @@
                 footerText="Riwayat Penduduk">
         </x-cards.stats-card>
     </div>
-    <div class="row fill-remaining-space">
-        <div class="col">
+    <div class="row flex-grow-1 mx-xl-5">
+        <div class="col flex-grow-1 d-flex flex-column overflow-auto mw-100">
             <div class="card card-chart">
                 <div class="card-header card-header-success">
-                    <h4 class="card-title">Jumlah Penduduk yang masuk</h4>
-                    <p class="card-category">2024</p>
+                    <span class="card-title">Jumlah Penduduk yang masuk</span>
+                    <span class="card-category">2024</span>
                 </div>
-                <div class="card-body">
-                    <div class="ct-chart"
+                <div class="card-body d-flex flex-column  overflow-auto">
+                    <div class="ct-chart flex-grow-1"
                          id="dailySalesChart">
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <div class="stats">
-                        <i class="material-icons">access_time</i> updated 4 minutes ago
                     </div>
                 </div>
             </div>
@@ -65,18 +60,20 @@
                             tension: 0
                         }),
                         low: 0,
-                        high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+                        high: 40, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
                         chartPadding: {
-                            top: 30,
-                            right: 15,
-                            bottom: 30,
-                            left: 15
+                            top: 25,
+                            right: 10,
+                            bottom: 25,
+                            left: 10
                         },
+                        width: '100%',
+                        height: '300px'
                     }
 
-                    const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+                    const dailySalesChart = new Chartist.LineChart('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
-                    md.startAnimationForLineChart(dailySalesChart);
+                    dailySalesChart.update();
                 }
             }
 
