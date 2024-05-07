05ee8ba1b70eae72f74eb0ca818dfbc53a4cfb8e
Fahridanaa <fahridanaaa@gmail.com>
2024-05-07 23:10:14 +0700

    fix: fix project configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import '../css/app.css';
+import '../sass/app.scss';
+import 'laravel-datatables-vite';
+import $ from 'jquery';
+import * as Chartist from 'chartist';
+
+window.Chartist = Chartist;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,36 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import 'node_modules/material-dashboard/assets/scss/material-dashboard.scss';`,
+            },
+        },
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    resolve: {
+        alias: {
+            'material-dashboard': 'material-dashboard/assets/js/material-dashboard.js',
+        },
+    },
+    optimizeDeps: {
+        include: ['jquery'],
+    },
+    build: {
+        outDir: 'public/build',
+    },
 });
