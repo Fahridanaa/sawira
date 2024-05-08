5e2248b4938031fed500c5e9e3f2ad5743c226ff
Fahridanaa <fahridanaaa@gmail.com>
2024-05-08 11:28:33 +0700

    feat: add Chart.js and remove Chartist for responsive design chart

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,3 @@ import './bootstrap';
 import '../css/app.css';
 import '../sass/app.scss';
 import 'laravel-datatables-vite';
-import $ from 'jquery';
-import * as Chartist from 'chartist';
-
-window.Chartist = Chartist;
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,46 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import 'node_modules/material-dashboard/assets/scss/material-dashboard.scss';`,
+export default defineConfig(({command, mode}) => {
+    const isProd = mode === 'production';
+
+    return {
+        define: {
+            'process.env': {},
+            'process.env.NODE_ENV': isProd ? '"production"' : '"development"',
+            global: 'window',
+            'global.jQuery': 'jquery',
+        },
+        plugins: [
+            laravel({
+                input: [
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import 'node_modules/material-dashboard/assets/scss/material-dashboard.scss';`,
+                },
             },
         },
-    },
-    server: {
-        hmr: {
-            host: 'localhost',
+        server: {
+            hmr: {
+                host: 'localhost',
+            },
         },
-    },
-    resolve: {
-        alias: {
-            'material-dashboard': 'material-dashboard/assets/js/material-dashboard.js',
+        resolve: {
+            alias: {
+                'material-dashboard': 'material-dashboard/assets/js/material-dashboard.js',
+            },
         },
-    },
-    optimizeDeps: {
-        include: ['jquery'],
-    },
-    build: {
-        outDir: 'public/build',
-    },
+        optimizeDeps: {
+            include: ['jquery'],
+        },
+        build: {
+            outDir: 'public/build',
+        },
+    };
 });
