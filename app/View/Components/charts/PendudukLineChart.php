e8024406c4ebf0a4d29d4a7628c24278f994eb3d
Fahridanaa <fahridanaaa@gmail.com>
2024-05-18 01:58:17 +0700

    feat: Refactor components and views for better data handling
    Updates were made to several chart components to include necessary data variables in the constructor, making data handling clearer and more straightforward. Also, the Blade syntax in views was simplified for cleaner markup and improved readability.

--- app/View/Components/charts/PendudukLineChart.php
+++ app/View/Components/charts/PendudukLineChart.php
@@ -8,19 +8,29 @@ use Illuminate\View\Component;
 
 class PendudukLineChart extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+	public string $cardTitle;
+	public string $chartId;
+	public array $labels;
+	public array $entryCitizenData;
+	public array $exitCitizenData;
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
-    {
-        return view('components.charts.penduduk-line-chart');
-    }
+	/**
+	 * Create a new component instance.
+	 */
+	public function __construct($cardTitle, $chartId, $labels, $entryCitizenData, $exitCitizenData)
+	{
+		$this->cardTitle = $cardTitle;
+		$this->chartId = $chartId;
+		$this->labels = $labels;
+		$this->entryCitizenData = $entryCitizenData;
+		$this->exitCitizenData = $exitCitizenData;
+	}
+
+	/**
+	 * Get the view / contents that represent the component.
+	 */
+	public function render(): View|Closure|string
+	{
+		return view('components.charts.penduduk-line-chart');
+	}
 }
--- app/View/Components/charts/UsiaPendudukPieChart.php
+++ app/View/Components/charts/UsiaPendudukPieChart.php
@@ -8,19 +8,25 @@ use Illuminate\View\Component;
 
 class UsiaPendudukPieChart extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+	public string $cardTitle;
+	public string $chartId;
+	public array $ageGroupData;
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
-    {
-        return view('components.charts.usia-penduduk-pie-chart');
-    }
+	/**
+	 * Create a new component instance.
+	 */
+	public function __construct($cardTitle, $chartId, $ageGroupData)
+	{
+		$this->cardTitle = $cardTitle;
+		$this->chartId = $chartId;
+		$this->ageGroupData = $ageGroupData;
+	}
+
+	/**
+	 * Get the view / contents that represent the component.
+	 */
+	public function render(): View|Closure|string
+	{
+		return view('components.charts.usia-penduduk-pie-chart');
+	}
 }
