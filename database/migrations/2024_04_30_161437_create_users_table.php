d5b38afb333dad1c803b928ae1dbeb2f575a1ebc
Fahridanaa <fahridanaaa@gmail.com>
2024-05-20 17:10:08 +0700

    fix: Refactor user roles and delete akun table
    The 'akun' table has been removed from the database and user roles have been updated in the 'users' table. The roles have been expanded and made more flexible by defining it as a constant array of roles. The seeder has also been updated to generate user records based on these roles.

--- database/migrations/2024_04_30_161437_create_users_table.php
+++ database/migrations/2024_04_30_161437_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 			$table->id('id_user');
 			$table->string('username', 25);
-			$table->enum('role', ['rw', 'rt', 'amil', 'warga'])->default('warga');
+			$table->enum('role', ['rw', 'rt1', 'rt2', 'rt3', 'rt4', 'rt5', 'rt6', 'rt7', 'rt8', 'rt9', 'rt10', 'rt11', 'rt12', 'rt13', 'rt14', 'rt15', 'rt16', 'rt17', 'rt18', 'amil', 'warga'])->default('warga');
 			$table->string('password', 255);
 			$table->timestamps();
 		});
