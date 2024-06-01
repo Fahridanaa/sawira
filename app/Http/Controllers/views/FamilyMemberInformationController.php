cace4fe407e9c5dba978d18595d54f01a4fe3a2a
Fahridanaa <fahridanaaa@gmail.com>
2024-06-02 05:10:01 +0700

    fix: rename information to memberInfo

---
+++ app/Http/Controllers/views/FamilyMemberInformationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\views;
+
+use App\DataTables\FamilyInformationDataTable;
+use App\Helpers\DateHelper;
+use App\Http\Controllers\Controller;
+use App\Models\KKModel;
+
+class FamilyMemberInformationController extends Controller
+{
+	public function index(FamilyInformationDataTable $dataTable)
+	{
+		$id_user = auth()->user()->id_user;
+		$id_kk = KKModel::where('id_user', $id_user)->first()->id_kk;
+		$dataTable->id_kk = $id_kk;
+		return $dataTable->render('pages.memberInfo.index');
+	}
+}
