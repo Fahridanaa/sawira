297f58a14976c584bef9e7b63ce7cb0d40173045
Fahridanaa <fahridanaaa@gmail.com>
2024-06-10 20:34:36 +0700

    fix: settings ui

--- resources/views/auth/settings.blade.php
+++ resources/views/auth/settings.blade.php
@@ -9,90 +9,104 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <table>
-                                <tr>
-                                    <td>Username</td>
-                                    <td>{{ auth()->user()->username }}</td>
-                                    @if(\App\Helpers\SidebarHelper::hasAnyRole(['warga']))
-                                        <td class="d-flex justify-content-end">
-                                            <button class="btn btn-primary detail-btn"
-                                                    data-toggle="modal"
-                                                    data-target="#updateUsername">Ubah
-                                            </button>
-                                        </td>
-                                    @endif
-                                </tr>
-                                @if(!\App\Helpers\SidebarHelper::hasAnyRole(['warga', 'rw', 'amil']))
-                                    <tr>
-                                        <td>Ketua RT</td>
-                                        <td>{{ $rt->ketua_rt }}</td>
-                                        <td class="d-flex justify-content-end">
-                                            <button class="btn btn-primary detail-btn"
-                                                    data-toggle="modal"
-                                                    data-target="#updateKetuaRT">Ubah
-                                            </button>
-                                        </td>
-                                    </tr>
-                                @endif
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Ubah Password</h4>
-                        </div>
-                        <div class="card-body">
-                            <form action="{{ route('auth.update.password') }}"
+                            <form action="{{ \App\Helpers\SidebarHelper::hasAnyRole(['warga', 'rw', 'amil']) ? route('auth.update.username') : route('rt.update.ketuart')}}"
                                   method="POST">
-                                @csrf
-                                {!! method_field('PUT') !!}
                                 <div class="row">
+                                    @csrf
+                                    {!! method_field('PUT') !!}
                                     <div class="col-12">
                                         <div class="form-group">
-                                            <label for="old_password">Password Lama</label>
-                                            <input type="password"
+                                            <label for="username">Username</label>
+                                            <input type="text"
                                                    class="form-control"
-                                                   id="old_password"
-                                                   name="old_password"
-                                                   required>
+                                                   value="{{ Auth()->user()->username }}"
+                                                   id="username"
+                                                   name="username"
+                                                   disabled>
                                         </div>
                                     </div>
+                                    @if(!\App\Helpers\SidebarHelper::hasAnyRole(['warga', 'rw', 'amil']))
+                                        <div class="col-12">
+                                            <div class="form-group">
+                                                <label for="new_ketuaRT">Ketua RT</label>
+                                                <input type="text"
+                                                       class="form-control"
+                                                       value="{{ $rt->ketua_rt ?? "FAZA" }}"
+                                                       id="new_ketuaRT"
+                                                       name="new_ketuaRT">
+                                            </div>
+                                        </div>
+                                    @endif
                                 </div>
                                 <div class="row">
-                                    <div class="col-6">
-                                        <div class="form-group">
-                                            <label for="new_password">Password Baru</label>
-                                            <input type="password"
-                                                   class="form-control"
-                                                   id="new_password"
-                                                   name="new_password"
-                                                   required>
-                                        </div>
-                                    </div>
-                                    <div class="col-6">
-                                        <div class="form-group">
-                                            <label for="confirm_password">Konfirmasi Password Baru</label>
-                                            <input type="password"
-                                                   class="form-control"
-                                                   id="confirm_password"
-                                                   name="confirm_password"
-                                                   required>
+                                    <div class="d-flex col-12 justify-content-end">
+                                        <div class="btn-group">
+                                            <button class="btn btn-primary ml-2"
+                                                    id="submit-button">Simpan
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
-
-                                <button type="submit"
-                                        class="btn btn-primary">Simpan
-                                </button>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Ubah Password</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('auth.update.password') }}"
+                              method="POST">
+                            @csrf
+                            {!! method_field('PUT') !!}
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="old_password">Password Lama</label>
+                                        <input type="password"
+                                               class="form-control"
+                                               id="old_password"
+                                               name="old_password"
+                                               required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label for="new_password">Password Baru</label>
+                                        <input type="password"
+                                               class="form-control"
+                                               id="new_password"
+                                               name="new_password"
+                                               required>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label for="confirm_password">Konfirmasi Password Baru</label>
+                                        <input type="password"
+                                               class="form-control"
+                                               id="confirm_password"
+                                               name="confirm_password"
+                                               required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="submit"
+                                    class="btn btn-primary">Simpan
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     </section>
     @if(\App\Helpers\SidebarHelper::hasAnyRole(['warga']))
