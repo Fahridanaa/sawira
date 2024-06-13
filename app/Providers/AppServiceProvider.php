c246d023daeb2a4071c100e6ec73baf832de71ff
Fahridanaa <fahridanaaa@gmail.com>
2024-06-13 22:44:07 +0700

    fix hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Yajra\DataTables\Html\Builder;
 
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
+		if (env('APP_ENV') === 'production') {
+			URL::forceScheme('https');
+		}
 		Builder::useVite();
 	}
 }
